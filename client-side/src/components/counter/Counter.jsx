9ec47c9d83e24cfc292f24a63f672b707c8c4ce9
Hasib <hasibhosen7612@gmail.com>
2023-11-29 18:43:45 +0600

    feat/counter section completed

--- client-side/src/components/counter/Counter.jsx
+++ client-side/src/components/counter/Counter.jsx
@@ -1,80 +1,18 @@
 import "../../assets/css/style.css";
 import "../../assets/css/responsive.css";
 import "../../assets/css/sections/global-settings.css";
+import CounterOne from "./CounterOne";
 
 const Counter = () => {
   return (
-    <div class="facts-section">
-      <div class="auto-container">
-        <div class="fact-counter">
-          <div class="row clearfix">
-            <div class="fact-block col-lg-3 col-md-6 col-sm-12">
-              <div class="inner clearfix">
-                <div class="fact-count">
-                  <div class="count-box">
-                    <span class="count-text" data-stop="120" data-speed="2000">
-                      0
-                    </span>
-                    <i>+</i>
-                  </div>
-                </div>
-                <div class="fact-title">
-                  Total <br />
-                  Destination
-                </div>
-              </div>
-            </div>
-
-            <div class="fact-block col-lg-3 col-md-6 col-sm-12">
-              <div class="inner clearfix">
-                <div class="fact-count">
-                  <div class="count-box">
-                    <span class="count-text" data-stop="500" data-speed="3000">
-                      0
-                    </span>
-                    <i>+</i>
-                  </div>
-                </div>
-                <div class="fact-title">
-                  Travel <br />
-                  Packages
-                </div>
-              </div>
-            </div>
-
-            <div class="fact-block col-lg-3 col-md-6 col-sm-12">
-              <div class="inner clearfix">
-                <div class="fact-count">
-                  <div class="count-box">
-                    <span class="count-text" data-stop="12" data-speed="2000">
-                      0
-                    </span>
-                    K<i>+</i>
-                  </div>
-                </div>
-                <div class="fact-title">
-                  Total <br />
-                  Travelers
-                </div>
-              </div>
-            </div>
-
-            <div class="fact-block col-lg-3 col-md-6 col-sm-12">
-              <div class="inner clearfix">
-                <div class="fact-count">
-                  <div class="count-box">
-                    <span class="count-text" data-stop="7" data-speed="2000">
-                      0
-                    </span>
-                    K<i>+</i>
-                  </div>
-                </div>
-                <div class="fact-title">
-                  Positive <br />
-                  Reviews
-                </div>
-              </div>
-            </div>
+    <div className="facts-section">
+      <div className="auto-container">
+        <div className="fact-counter">
+          <div className="row clearfix">
+            <CounterOne number={120} title="Total<br/>Destination" />
+            <CounterOne number={500} title="Travel<br/>Packages" />
+            <CounterOne number={12} suffix="K" title="Total<br/>Travelers" />
+            <CounterOne number={7} suffix="K" title="Positive<br/>Reviews" />
           </div>
         </div>
       </div>
