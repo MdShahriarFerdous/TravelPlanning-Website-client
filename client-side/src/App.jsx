b02d3ef38548deef9745cfe0bb2ccd8543ecae1a
Miraz Uddin <mirazuddin0623@gmail.com>
2023-11-29 14:17:52 +0600

    Active / Inactive functionality added in Navbar

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -2,14 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import BlogsListPage from "./pages/blogs/BlogsListPage";
 import BlogDetailsPage from "./pages/blogs/BlogDetailsPage";
+import TripsListPage from "./pages/trips/TripsListPage";
+import HotelsListPage from "./pages/hotels/HotelsListPage";
+import FlightsListPage from "./pages/flights/FlightsListPage";
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
+				<Route path="/trips" element={<TripsListPage />} />
+				<Route path="/flights" element={<FlightsListPage />} />
 				<Route path="/blogs" element={<BlogsListPage />} />
 				<Route path="/blogs/:blogId" element={<BlogDetailsPage />} />
+				<Route path="/hotels" element={<HotelsListPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ client-side/src/pages/hotels/HotelsListPage.jsx
@@ -0,0 +1,15 @@
+import AppLayout from "../../components/applayout/AppLayout";
+
+const HotelsListPage = () => {
+  return (
+    <AppLayout>
+      <div className="sidebar-container blog-page">
+        <div className="auto-container">
+          <div className="row clearfix">All Hotels List Here</div>
+        </div>
+      </div>
+    </AppLayout>
+  );
+};
+
+export default HotelsListPage;
