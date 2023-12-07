e9c738bf790b4fd8f708b875c83927a51ef4ed43
Miraz Uddin <mirazuddin0623@gmail.com>
2023-12-07 16:07:30 +0600

    Home page Blog Sections Dynamic Done

--- client-side/src/components/blogs/HomeView/FeaturedBlogs.jsx
+++ client-side/src/components/blogs/HomeView/FeaturedBlogs.jsx
@@ -1,127 +1,61 @@
-import Background1 from "../../../assets/images/background/bg-gradient-9.png";
-import Post1 from "../../../assets/images/resources/posts/post-1.jpg";
-import Insta1 from "../../../assets/images/resources/instagram/insta-5.jpg";
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-import Slider from "react-slick";
-import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  blogsInHomeList,
+  blogsGalleryList,
+} from "../../../backend-services/blogsApi";
+import ScreenLoader from "../../screenloader/ScreenLoader";
+import FeaturedSection from "./section/FeaturedSection";
+import GallerySection from "./section/GallerySection";
 
 export default function FeaturedBlogs() {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnHover: false,
-  };
+  const [featuredBlogs, setFeaturedBlogs] = useState([]);
+  const [galleryBlogs, setGalleryBlogs] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isGalleryBlogsLoading, setIsGalleryBlogsLoading] = useState(false);
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      const res = await blogsInHomeList();
+      if (res?.data) {
+        const { blogs } = res?.data || {};
+        setIsLoading(false);
+        setFeaturedBlogs(blogs);
+      }
+    })();
+    (async () => {
+      setIsGalleryBlogsLoading(true);
+      const res = await blogsGalleryList();
+      if (res?.data) {
+        const { blogs } = res?.data || {};
+        setIsGalleryBlogsLoading(false);
+        setGalleryBlogs(blogs);
+      }
+    })();
+  }, []);
   return (
     <>
-      <div className="news-section">
-        <div className="auto-container">
-          <div className="title-box centered">
-            <div className="subtitle">Updates</div>
-            <h2>
-              <span>From Our Blog</span>
-            </h2>
-          </div>
-
-          <div className="news-box">
-            <div className="bg-grad-left">
-              <img src={Background1} alt="" title="" />
-            </div>
-            <ResponsiveMasonry
-              columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 2 }}
-              className="masonry-container row clearfix"
-            >
-              <Masonry gutter="1.5rem">
-                <div className="news-block-one">
-                  <div className="inner-box">
-                    <div
-                      className="image-layer"
-                      style={{ backgroundImage: `url(${Post1})` }}
-                    ></div>
-                    <div className="over-box">
-                      <ul className="info clearfix">
-                        <li>
-                          <a href="#">
-                            <i className="fa-solid fa-folder"></i> Adventure
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="fa-solid fa-clock"></i> June 17, 2022
-                          </a>
-                        </li>
-                      </ul>
-                      <h3>
-                        <NavLink to={`/blogs/1`}>
-                          Off the Beaten Path Travel Adventures
-                        </NavLink>
-                      </h3>
-                    </div>
-                  </div>
+      {isLoading || isGalleryBlogsLoading ? (
+        <ScreenLoader />
+      ) : (
+        <>
+          {featuredBlogs && featuredBlogs.length > 0 && (
+            <div className="news-section">
+              <div className="auto-container">
+                <div className="title-box centered">
+                  <div className="subtitle">Updates</div>
+                  <h2>
+                    <span>From Our Blog</span>
+                  </h2>
                 </div>
-                <div className="news-block-one">
-                  <div className="inner-box">
-                    <div
-                      className="image-layer"
-                      style={{ backgroundImage: `url(${Post1})` }}
-                    ></div>
-                    <div className="over-box">
-                      <ul className="info clearfix">
-                        <li>
-                          <a href="#">
-                            <i className="fa-solid fa-folder"></i> Adventure
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="fa-solid fa-clock"></i> June 17, 2022
-                          </a>
-                        </li>
-                      </ul>
-                      <h3>
-                        <NavLink to={`/blogs/1`}>
-                          Off the Beaten Path Travel Adventures
-                        </NavLink>
-                      </h3>
-                    </div>
-                  </div>
-                </div>
-              </Masonry>
-            </ResponsiveMasonry>
-          </div>
-        </div>
-      </div>
-      <div className="insta-section">
-        <div className="insta-feed">
-          <div className="carousel-container">
-            <div className="carousel-box">
-              <div className="insta-carousel">
-                <Slider {...settings}>
-                  {Array.apply(null, { length: 8 }).map((e, i) => (
-                    <div key={i} className="insta-block">
-                      <div className="image">
-                        <span className="img">
-                          <a
-                            href={Insta1}
-                            className="lightbox-image"
-                            data-fancybox="insta-gallery"
-                          >
-                            <img src={Insta1} alt="" />
-                          </a>
-                        </span>
-                      </div>
-                    </div>
-                  ))}
-                </Slider>
+                <FeaturedSection featuredBlogs={featuredBlogs} />
               </div>
             </div>
-          </div>
-        </div>
-      </div>
+          )}
+          {galleryBlogs && galleryBlogs.length > 0 && (
+            <GallerySection galleryBlogs={galleryBlogs} />
+          )}
+        </>
+      )}
     </>
   );
 }
---
+++ client-side/src/components/blogs/HomeView/section/GallerySection.jsx
@@ -0,0 +1,88 @@
+/* eslint-disable react/prop-types */
+import Slider from "react-slick";
+
+export default function GallerySection({ galleryBlogs }) {
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 1366,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="insta-section">
+      <div className="insta-feed">
+        <div className="carousel-container">
+          <div className="carousel-box">
+            <div className="insta-carousel">
+              <Slider {...settings}>
+                {galleryBlogs.map((gallery) => {
+                  const { _id, galleryImage } = gallery || {};
+                  return (
+                    <div key={_id} className="insta-block">
+                      <div className="image">
+                        <span className="img">
+                          <a
+                            href={galleryImage}
+                            className="lightbox-image"
+                            data-fancybox="insta-gallery"
+                          >
+                            <img src={galleryImage} alt="" />
+                          </a>
+                        </span>
+                      </div>
+                    </div>
+                  );
+                })}
+              </Slider>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
