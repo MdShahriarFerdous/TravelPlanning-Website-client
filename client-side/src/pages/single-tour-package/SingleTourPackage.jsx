57162965d217566270cce47c8792ac97a2cc1f3d
Shahriar Ferdous <shahriarferdous2233@gmail.com>
2023-12-08 19:20:38 +0600

    [Updated] tour-package

--- client-side/src/pages/single-tour-package/SingleTourPackage.jsx
+++ client-side/src/pages/single-tour-package/SingleTourPackage.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
-
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { HiOutlineBookmark, HiBookmark } from "react-icons/hi2";
+import { TiTick } from "react-icons/ti";
+// <HiBookmark />   <HiOutlineBookmark /> <TiTick />
 import { useParams } from "react-router-dom";
 import { TourByIdAPI } from "../../backend-services/api";
 import { toast } from "react-toastify";
@@ -11,6 +14,7 @@ import "../../assets/css/responsive.css";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap";
+import "./singletour.css";
 
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
@@ -19,6 +23,14 @@ import ScreenLoader from "./../../components/screenloader/ScreenLoader";
 const SingleTourPackage = () => {
 	const [tourDetails, setTourDetails] = useState({});
 	const [loading, setLoading] = useState(true);
+
+	const [toggle, setToggle] = useState(false);
+	const [bookmarkToggle, setBookmarkToggle] = useState(false);
+	const [durationToggle, setDurationToggle] = useState(false);
+	const [distanceToggle, setDistanceToggle] = useState(false);
+	const [descriptionToggle, setDescriptionToggle] = useState(false);
+	const [additionalToggle, setAdditionalToggle] = useState(false);
+
 	const { tourInfoId } = useParams();
 
 	useEffect(() => {
@@ -43,6 +55,25 @@ const SingleTourPackage = () => {
 		fetchTourInfo();
 	}, [tourInfoId]);
 
+	const handleToggleClick = () => {
+		setToggle(!toggle);
+	};
+	const handleBookmarkClick = () => {
+		setBookmarkToggle(!bookmarkToggle);
+	};
+	const handleDurationToggle = () => {
+		setDurationToggle(!durationToggle);
+	};
+	const handleDistanceToggle = () => {
+		setDistanceToggle(!distanceToggle);
+	};
+	const handleDescriptionToggle = () => {
+		setDescriptionToggle(!descriptionToggle);
+	};
+	const handleAdditionalToggle = () => {
+		setAdditionalToggle(!additionalToggle);
+	};
+	// console.log(tourDetails);
 	return (
 		<>
 			{loading ? (
@@ -51,13 +82,29 @@ const SingleTourPackage = () => {
 				<>
 					<Navbar />
 					<div
-						className="container"
+						className="container single-tour"
 						style={{ marginTop: "180px", marginBottom: "180px" }}
 					>
 						<div className="container">
-							<h3 className="mb-4">
-								{tourDetails?.getTourInfo?.title}
-							</h3>
+							<div className="d-flex  mb-4">
+								<h3
+									className="mb-4"
+									style={{ letterSpacing: "0.8px" }}
+								>
+									{tourDetails?.getTourInfo?.title}
+								</h3>
+								<span
+									className="span-bookmark"
+									onClick={handleBookmarkClick}
+								>
+									{bookmarkToggle ? (
+										<HiBookmark className="bookmark fill-bookmark" />
+									) : (
+										<HiOutlineBookmark className="bookmark" />
+									)}
+								</span>
+							</div>
+
 							{tourDetails &&
 								tourDetails?.getTourInfo?.images && (
 									<div className="row g-2">
@@ -108,32 +155,233 @@ const SingleTourPackage = () => {
 						<div className="row mt-5 g-0">
 							<div class="col-lg-8 ps-3">
 								<div className="card p-4 mx-2">
-									<p className="fw-bold">About</p>
+									<h6 className="fw-bold">About</h6>
 									<p className="text-muted">
 										{tourDetails?.getTourInfo?.about}
 									</p>
 									<hr></hr>
-									<p className="fw-bold">Locations</p>
-									{tourDetails?.getTourInfo?.locations.map(
-										(area) => (
-											<>
-												<ul>
-													<li>{area}</li>
-												</ul>
-											</>
-										)
+
+									<h6 className="fw-bold mb-2">
+										Locations
+										<button
+											style={{
+												background: "transparent",
+											}}
+											onClick={handleToggleClick}
+											className="toggle-button"
+										>
+											{toggle ? (
+												<IoIosArrowUp className="arrow" />
+											) : (
+												<IoIosArrowDown className="arrow" />
+											)}
+										</button>
+									</h6>
+									{toggle && (
+										<ul>
+											{tourDetails?.getTourInfo?.locations.map(
+												(area, index) => (
+													<li key={index + 1}>
+														<p>
+															{" "}
+															<TiTick className="tick" />
+															{area}
+														</p>
+													</li>
+												)
+											)}
+										</ul>
 									)}
 									<hr></hr>
-									<p className="fw-bold">Durations</p>
-									<p>{tourDetails?.getTourInfo?.duration}</p>
+
+									<h6 className="fw-bold">
+										Durations
+										<button
+											style={{
+												background: "transparent",
+											}}
+											onClick={handleDurationToggle}
+											className="toggle-button"
+										>
+											{durationToggle ? (
+												<IoIosArrowUp className="arrow" />
+											) : (
+												<IoIosArrowDown className="arrow" />
+											)}
+										</button>
+									</h6>
+									{durationToggle && (
+										<p>
+											{tourDetails?.getTourInfo?.duration}
+										</p>
+									)}
 									<hr></hr>
-									<p className="fw-bold">Distance</p>
-									<p>{tourDetails?.getTourInfo?.distance}</p>
+
+									<h6 className="fw-bold">
+										Distance
+										<button
+											style={{
+												background: "transparent",
+											}}
+											onClick={handleDistanceToggle}
+											className="toggle-button"
+										>
+											{distanceToggle ? (
+												<IoIosArrowUp className="arrow" />
+											) : (
+												<IoIosArrowDown className="arrow" />
+											)}
+										</button>
+									</h6>
+									{distanceToggle && (
+										<p>
+											{tourDetails?.getTourInfo?.distance}
+										</p>
+									)}
 									<hr></hr>
-									<p className="fw-bold">Group Size</p>
-									<p>
-										{tourDetails?.getTourInfo?.maxGroupSize}
-									</p>
+
+									<h6 className="fw-bold">
+										Detailed Description
+										<button
+											style={{
+												background: "transparent",
+											}}
+											onClick={handleDescriptionToggle}
+											className="toggle-button"
+										>
+											{descriptionToggle ? (
+												<IoIosArrowUp className="arrow" />
+											) : (
+												<IoIosArrowDown className="arrow" />
+											)}
+										</button>
+									</h6>
+									{descriptionToggle && (
+										<>
+											{tourDetails?.description.map(
+												(item, index) => (
+													<div key={index + 1}>
+														<p>{item?.details}</p>
+														<p className="p-bold">
+															Locations that will
+															be covered in this
+															tour are:
+														</p>
+														{item?.coveredLocations.map(
+															(location, i) => (
+																<p key={i + 1}>
+																	<TiTick />
+																	{location}
+																</p>
+															)
+														)}
+														<p className="p-bold">
+															Rooms will be
+															allotted from the
+															following Resorts
+															only:
+														</p>
+														<p>
+															{" "}
+															Premium Package:{" "}
+															{
+																item?.premiumResorts
+															}
+														</p>
+														<p>
+															{" "}
+															Economy Package:{" "}
+															{
+																item?.economyResorts
+															}
+														</p>
+														<p className="p-bold">
+															Max number:{" "}
+															{
+																tourDetails
+																	?.getTourInfo
+																	?.maxGroupSize
+															}{" "}
+														</p>
+
+														<p className="p-bold">
+															Food Menu
+														</p>
+														{tourDetails?.foodmenu?.map(
+															(food, index) => (
+																<>
+																	<p
+																		key={
+																			index +
+																			1
+																		}
+																	>
+																		Day
+																		{
+																			food.dayNo
+																		}
+																	</p>
+																	<ul>
+																		<li>
+																			<p>
+																				Breakfast:{" "}
+																				{food.breakfast.join()}
+																			</p>
+																		</li>
+																		<li>
+																			<p>
+																				Lunch:{" "}
+																				{food.lunch.join()}
+																			</p>
+																		</li>
+																		<li>
+																			<p>
+																				Dinner:{" "}
+																				{food.dinner.join()}
+																			</p>
+																		</li>
+																	</ul>
+																</>
+															)
+														)}
+													</div>
+												)
+											)}
+										</>
+									)}
+									<hr></hr>
+
+									<h6 className="fw-bold">
+										Additional Information
+										<button
+											style={{
+												background: "transparent",
+											}}
+											onClick={handleAdditionalToggle}
+											className="toggle-button"
+										>
+											{additionalToggle ? (
+												<IoIosArrowUp className="arrow" />
+											) : (
+												<IoIosArrowDown className="arrow" />
+											)}
+										</button>
+									</h6>
+									{additionalToggle &&
+										tourDetails?.getTourInfo?.additionalInfo.map(
+											(info, index) => (
+												<>
+													<ul key={index + 1}>
+														<li>
+															<p>
+																<TiTick />
+																{info}
+															</p>
+														</li>
+													</ul>
+												</>
+											)
+										)}
 								</div>
 							</div>
 
