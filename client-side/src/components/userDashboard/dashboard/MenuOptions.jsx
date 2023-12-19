263c14d2d54cbea8cdf89b93ae2c1e3b1d4f59a6
Shahriar Ferdous <shahriarferdous2233@gmail.com>
2023-12-19 15:32:55 +0600

    [Updated]-Dashboard

--- client-side/src/components/userDashboard/dashboard/MenuOptions.jsx
+++ client-side/src/components/userDashboard/dashboard/MenuOptions.jsx
@@ -8,52 +8,50 @@ import { Link } from "react-router-dom";
 
 const MenuOptions = () => {
 	return (
-		<div
-			className="row  mb-5 main-options-block"
-			style={{ marginTop: "72px" }}
-		>
-			<h3 className="mb-4">Step Ahead Your Journey</h3>
-
-			<div className="col-lg-3 options-block">
-				<div className="card shadow-sm p-3 tour-card-design">
-					<p>Check Latest Tour Packages</p>
-					<div className="d-flex justify-content-center align-items-center icon-block">
-						<FaTree className="tree-icon" />
+		<div className="container-fluid mt-5 pt-5">
+			<h3 className="mb-5">Step Ahead Your Journey</h3>
+			<div className="row mb-5 main-options-block">
+				<div className="col-lg-3 options-block">
+					<div className="card shadow-sm p-3 tour-card-design">
+						<p>Check Latest Tour Packages</p>
+						<div className="d-flex justify-content-center align-items-center icon-block">
+							<FaTree className="tree-icon" />
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<div className="col-lg-3 options-block">
-				<Link to="/hotels">
-					<div className="card shadow-sm p-3 hotel-card-design">
-						<p>Book Hotel for Your Vacation</p>
-						<div className="d-flex justify-content-center align-items-center icon-block">
-							<FaHotel className="hotel-icon" />
+				<div className="col-lg-3 options-block">
+					<Link to="/hotels">
+						<div className="card shadow-sm p-3 hotel-card-design">
+							<p>Book Hotel for Your Vacation</p>
+							<div className="d-flex justify-content-center align-items-center icon-block">
+								<FaHotel className="hotel-icon" />
+							</div>
 						</div>
-					</div>
-				</Link>
-			</div>
+					</Link>
+				</div>
 
-			<div className="col-lg-3 options-block">
-				<Link to="/flights">
-					<div className="card shadow-sm px-4 py-3 flight-card-design">
-						<p>Check Here for Flights</p>
-						<div className="d-flex justify-content-center align-items-center icon-block">
-							<FaPlaneDeparture className="flight-icon" />
+				<div className="col-lg-3 options-block">
+					<Link to="/flights">
+						<div className="card shadow-sm px-4 py-3 flight-card-design">
+							<p>Searching for Flights? Check Here</p>
+							<div className="d-flex justify-content-center align-items-center icon-block">
+								<FaPlaneDeparture className="flight-icon" />
+							</div>
 						</div>
-					</div>
-				</Link>
-			</div>
+					</Link>
+				</div>
 
-			<div className="col-lg-3 options-block">
-				<Link to="/trips">
-					<div className="card shadow-sm p-3 itinerary-card-design">
-						<p>Make Your Itinerary Here</p>
-						<div className="d-flex justify-content-center align-items-center icon-block">
-							<FaTasks className="task-icon" />
+				<div className="col-lg-3 options-block">
+					<Link to="/trips">
+						<div className="card shadow-sm p-3 itinerary-card-design">
+							<p>Make Your Itinerary Here</p>
+							<div className="d-flex justify-content-center align-items-center icon-block">
+								<FaTasks className="task-icon" />
+							</div>
 						</div>
-					</div>
-				</Link>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
--- client-side/src/pages/user/UserDashboard.jsx
+++ client-side/src/pages/user/UserDashboard.jsx
@@ -3,23 +3,23 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap";
 import UserSideNavbar from "../../components/userDashboard/navbar/UserSideNavbar";
 import Dashboard from "../../components/userDashboard/dashboard/Dashboard";
-import "../../components/userDashboard/commonCSS/common.css"
+import "../../components/userDashboard/commonCSS/common.css";
 
 const UserDashboard = () => {
-  return (
-    <div className="parent_content">
-      <div className="container-fluids">
-        <div className="row">
-          <div className="col-4">
-            <UserSideNavbar />
-          </div>
-          <div className="col-8">
-            <Dashboard />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="parent_content">
+			<div className="container-fluids">
+				<div className="row">
+					<div className="col-lg-3">
+						<UserSideNavbar />
+					</div>
+					<div className="col-lg-9">
+						<Dashboard />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default UserDashboard;
